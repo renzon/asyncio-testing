834c3d695dff64a142589beb06028dddad6a3326
renzon <renzon@gmail.com>
2017-07-06 17:57:15 -0300

    Implemented Async Mock with pytest, mocker and fixture

--- test_async_receive.py
+++ test_async_receive.py
@@ -2,12 +2,16 @@
 import asyncio
 import unittest
 from unittest import mock
+
+import pytest as pytest
+
 from async_receive import receive
 
 
 def _run(coro):
     """Run the given coroutine."""
     return asyncio.get_event_loop().run_until_complete(coro)
+
 
 def AsyncMock(*args, **kwargs):
     """Create an async function mock."""
@@ -30,15 +34,25 @@ class TestReceive(unittest.TestCase):
         from async_receive import send_to_client
         send_to_client.mock.assert_called_once_with('PONG', 'data')
 
-    @mock.patch('async_receive.send_to_client', new=AsyncMock())
-    @mock.patch('async_receive.trigger_event',
-                new=AsyncMock(return_value='my response'))
-    def test_message(self):
-        with mock.patch('async_receive.send_to_client', new=AsyncMock()) as send_to_client:
-            with mock.patch('async_receive.trigger_event', new=AsyncMock(return_value='my response')) as trigger_event:
-                _run(receive('MESSAGE', 'data'))
-                trigger_event.mock.assert_called_once_with('message', 'data')
-                send_to_client.mock.assert_called_once_with('MESSAGE', 'my response')
+
+@pytest.fixture
+def send_to_client(mocker):
+    send_to_client_mock = AsyncMock()
+    mocker.patch('async_receive.send_to_client', new=send_to_client_mock)
+    return send_to_client_mock
+
+
+@pytest.fixture
+def trigger_event(mocker):
+    trigger_event_mock = AsyncMock(return_value='my response')
+    mocker.patch('async_receive.trigger_event', new=trigger_event_mock)
+    return trigger_event_mock
+
+
+def test_message(send_to_client, trigger_event):
+    _run(receive('MESSAGE', 'data'))
+    trigger_event.mock.assert_called_once_with('message', 'data')
+    send_to_client.mock.assert_called_once_with('MESSAGE', 'my response')
 
 
 if __name__ == '__main__':
